b20925c7dd1ec0895bf6b4ee621d879c3d94c72b
caro553 <57499404+caro553@users.noreply.github.com>
2023-02-26 17:06:14 +0100

    Update TopBar.js

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import TopBar from './TopBar';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import LoginScreen from './LoginScreen';
+import BurgerMenu from './BurgerMenu';
 
 export default function TopBar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <View style={styles.container}>
-      <TopBar />
-      <LoginScreen />
+      <TopBar>
+        <TouchableOpacity onPress={toggleMenu}>
+          <Text>Menu</Text>
+        </TouchableOpacity>
+      </TopBar>
+      <BurgerMenu visible={showMenu} onClose={toggleMenu} />
+      {/* Contenu de la page */}
     </View>
   );
 }
