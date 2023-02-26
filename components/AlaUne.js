99cf051aa92d51f0657956bd1dbb3f7a6a431eba
caro553 <57499404+caro553@users.noreply.github.com>
2023-02-26 20:19:49 +0100

    AlaUne

--- components/AlaUne.js
+++ components/AlaUne.js
@@ -1,36 +1,79 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { WebView } from 'react-native-webview';
+import TopBar from './TopBar';
 
 export default function AlaUne() {
   return (
     <View style={styles.container}>
-      <Ionicons name="menu" size={24} color="white" style={styles.menuIcon} />
-      <Text style={styles.title}>Mon application</Text>
-      <Ionicons name="person" size={24} color="white" style={styles.profileIcon} />
+      <View style={styles.topBar}>
+        <TopBar />
+      </View>
+
+      <View style={styles.videoContainer}>
+        <WebView
+          source={{ uri: 'https://player.twitch.tv/?video=v1748916709&autoplay=false' }}
+          style={styles.video}
+        />
+      </View>
+      <View style={styles.actionsContainer}>
+        <View style={styles.actionButton}>
+          <Text style={styles.likeIcon}>Like</Text>
+        </View>
+        <View style={styles.actionButton}>
+          <Text style={styles.commentIcon}>Comment</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'purple',
-    height: 64,
-    paddingHorizontal: 16,
-  },
-  menuIcon: {
-    marginRight: 16,
-  },
-  title: {
     flex: 1,
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#6441A4', // nouvelle couleur de fond correspondant à la couleur de Twitch
   },
-  profileIcon: {
-    marginLeft: 16,
+  topBar: {
+    backgroundColor: '#5f5f5f',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  videoContainer: {
+    width: '80%',
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  video: {
+    width: '100%',
+    aspectRatio: 2,
+  },
+  actionsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  actionButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 80,
+    height: 30,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    marginHorizontal: 5,
+  },
+  likeIcon: {
+    fontWeight: 'bold',
+  },
+  commentIcon: {
+    fontWeight: 'bold',
   },
 });
--- components/TopBar.js
+++ components/TopBar.js
@@ -1,31 +1,94 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
-import TopBar from './TopBar';
-import BurgerMenu from './BurgerMenu';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function MyPage() {
+export default function TopBar() {
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
 
+  const handlePage1 = () => {
+    // Ajouter l'action à effectuer lorsqu'on clique sur l'icône de la page 1
+  };
+
+  const handlePage2 = () => {
+    // Ajouter l'action à effectuer lorsqu'on clique sur l'icône de la page 2
+  };
+
+  const handlePage3 = () => {
+    // Ajouter l'action à effectuer lorsqu'on clique sur l'icône de la page 3
+  };
+
   return (
     <View style={styles.container}>
-      <TopBar>
+      <View style={styles.burgerContainer}>
         <TouchableOpacity onPress={toggleMenu}>
-          <Text>Menu</Text>
+          <Ionicons name="menu" size={24} color="white" style={styles.menuIcon} />
         </TouchableOpacity>
-      </TopBar>
-      <BurgerMenu visible={showMenu} onClose={toggleMenu} />
-      {/* Contenu de la page */}
+      </View>
+      <Text style={styles.title}>Mon application</Text>
+      <Ionicons name="person" size={24} color="white" style={styles.profileIcon} />
+      {showMenu && (
+        <View style={styles.menuContainer}>
+          <TouchableOpacity onPress={handlePage1}>
+            <Ionicons name="ios-home" size={24} color="black" style={styles.menuIcon} />
+            <Text style={styles.menuText}>Page 1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handlePage2}>
+            <Ionicons name="ios-rocket" size={24} color="black" style={styles.menuIcon} />
+            <Text style={styles.menuText}>Page 2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handlePage3}>
+            <Ionicons name="ios-settings" size={24} color="black" style={styles.menuIcon} />
+            <Text style={styles.menuText}>Page 3</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#6441a5',
+    height: 64,
+    paddingHorizontal: 16,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  burgerContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    justifyContent: 'center',
+    zIndex: 1,
+  },
+  menuIcon: {
+    marginLeft: 16,
+  },
+  title: {
     flex: 1,
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  profileIcon: {
+    marginLeft: 16,
+  },
+  menuContainer: {
+    position: 'absolute',
+    top: 64,
+    left: 0,
     backgroundColor: 'white',
+    padding: 16,
+    elevation: 5,
+    flex: 1,
+    width: '100%',
+    bottom: 0,
   },
 });
