2aaa6e8eed677047a2b5fa9eee10c825da751494
HugoAndre15 <hugo.andre1501@gmail.com>
2023-05-10 11:50:04 +0200

    change hugo event page

--- components/AlaUne.js
+++ components/AlaUne.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import { View, ScrollView, TouchableOpacity, Text, Image, Linking, StyleSheet } from 'react-native';
+import React from "react";
+import {
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+  Image,
+  Linking,
+  StyleSheet,
+} from "react-native";
+import { WebView } from "react-native-webview";
+import { Dimensions } from "react-native";
+import TopBar from "./TopBar";
+import BottomBar from "./BottomBar";
+import { auth } from "./firebaseConfig";
 
-import { WebView } from 'react-native-webview';
-import TopBar from './TopBar';
-import BottomBar from './BottomBar';
+const gradientColors = ["purple", "yellow"];
+const screenWidth = Dimensions.get("window").width;
+function renderBorder(colors) {
+  const numSegments = colors.length - 1;
+  const segmentWidth = screenWidth / numSegments;
+  return colors.map((color, index) => (
+    <View
+      key={index}
+      style={[
+        styles.borderSegment,
+        { backgroundColor: color, width: segmentWidth },
+      ]}
+    />
+  ));
+}
+
 export default function AlaUne() {
   return (
     <View style={styles.container}>
@@ -13,61 +39,101 @@ export default function AlaUne() {
       {/* Contenu de la page */}
       <BottomBar />
 
-      <View style={styles.videoContainer} zIndex={0}>
-        <WebView
-          source={{ uri: 'https://player.twitch.tv/?video=v1748916709&autoplay=false' }}
-          style={styles.video}
-        />
-      </View>
-      <View style={styles.actionsContainer}>
-        <TouchableOpacity style={styles.actionButton} zIndex={0}>
-          <Image source={require('./like.png')} style={styles.icon} />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.actionButton}>
-          <Image source={require('./comment.png')} style={styles.icon} />
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.descriptionContainer} zIndex={0}>
-        <Text style={styles.descriptionText}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor pharetra ipsum in bibendum.
-          Pellentesque sed augue non eros congue tincidunt. Nullam bibendum faucibus nulla eu rhoncus.
-        </Text>
-      </View>
-
       <ScrollView style={styles.scrollView}>
-        <TouchableOpacity style={styles.twitchContainer} onPress={() => Linking.openURL('https://www.youtube.com/watch?v=r67zVQK7zE0')}>
-          <Image source={require('./rectangle.png')} style={styles.twitchIcon} />
+        <View style={styles.ProfilUne} zIndex={0}>
+          <Image source={require("./couronne.png")} style={styles.couronne} />
+          <Image
+            source={require("./demonslayer.jpg")}
+            style={[styles.logoducomte, styles.imageWithBorder]}
+          />
+          <View style={styles.starContainer}>
+            <View style={styles.starShape}>
+              <Text style={styles.starText}>
+                Découvrez le streamer à la une !
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.descriptionContainer} zIndex={0}>
+          <Text style={styles.descriptionText}>
+            Daksu est un jeune streamer passionné des jeux Fromsoftare et de
+            l’univers Pokémon !! Découvrez le monde de Daksu !
+          </Text>
+          {renderBorder(gradientColors)}
+        </View>
+
+        <Text style={styles.scrollText}>En ce moment sur Twitch :</Text>
+        <TouchableOpacity
+          style={styles.twitchContainer}
+          onPress={() =>
+            Linking.openURL("https://www.youtube.com/watch?v=r67zVQK7zE0")
+          }
+        >
+          <Image
+            source={require("./rectangle.png")}
+            style={styles.twitchIcon}
+          />
           <View style={styles.twitchTextContainer}>
             <Text style={styles.twitchTitle}>Deuxième vidéo sur Youtube</Text>
             <Text style={styles.twitchChannel}>Chaîne Youtube 2</Text>
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.twitchContainer} onPress={() => Linking.openURL('https://www.twitch.tv')}>
-          <Image source={require('./rectangle.png')} style={styles.twitchIcon} />
+        <TouchableOpacity
+          style={styles.twitchContainer}
+          onPress={() => Linking.openURL("https://www.twitch.tv")}
+        >
+          <Image
+            source={require("./rectangle.png")}
+            style={styles.twitchIcon}
+          />
           <View style={styles.twitchTextContainer}>
             <Text style={styles.twitchTitle}>Stream en direct sur Twitch</Text>
             <Text style={styles.twitchChannel}>Chaîne Twitch 1</Text>
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.twitchContainer} onPress={() => Linking.openURL('https://www.youtube.com/watch?v=ZXsQAXx_ao0')}>
-          <Image source={require('./rectangle.png')} style={styles.twitchIcon} />
+        <TouchableOpacity
+          style={styles.twitchContainer}
+          onPress={() =>
+            Linking.openURL("https://www.youtube.com/watch?v=ZXsQAXx_ao0")
+          }
+        >
+          <Image
+            source={require("./rectangle.png")}
+            style={styles.twitchIcon}
+          />
           <View style={styles.twitchTextContainer}>
             <Text style={styles.twitchTitle}>Première vidéo sur Youtube</Text>
             <Text style={styles.twitchChannel}>Chaîne Youtube 1</Text>
           </View>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.twitchContainer} onPress={() => Linking.openURL('https://www.youtube.com/watch?v=ZXsQAXx_ao0')}>
-          <Image source={require('./rectangle.png')} style={styles.twitchIcon} />
+        <TouchableOpacity
+          style={styles.twitchContainer}
+          onPress={() =>
+            Linking.openURL("https://www.youtube.com/watch?v=ZXsQAXx_ao0")
+          }
+        >
+          <Image
+            source={require("./rectangle.png")}
+            style={styles.twitchIcon}
+          />
           <View style={styles.twitchTextContainer}>
             <Text style={styles.twitchTitle}>Première vidéo sur Youtube</Text>
             <Text style={styles.twitchChannel}>Chaîne Youtube 1</Text>
           </View>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.twitchContainer} onPress={() => Linking.openURL('https://www.youtube.com/watch?v=ZXsQAXx_ao0')}>
-          <Image source={require('./rectangle.png')} style={styles.twitchIcon} />
+        <TouchableOpacity
+          style={styles.twitchContainer}
+          onPress={() =>
+            Linking.openURL("https://www.youtube.com/watch?v=ZXsQAXx_ao0")
+          }
+        >
+          <Image
+            source={require("./rectangle.png")}
+            style={styles.twitchIcon}
+          />
           <View style={styles.twitchTextContainer}>
             <Text style={styles.twitchTitle}>Première vidéo sur Youtube</Text>
             <Text style={styles.twitchChannel}>Chaîne Youtube 1</Text>
@@ -83,48 +149,50 @@ export default function AlaUne() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#6441A4', // nouvelle couleur de fond correspondant à la couleur de Twitch
-    position: 'relative',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#6441A4", // nouvelle couleur de fond correspondant à la couleur de Twitch
+    position: "relative",
 
     zIndex: 0, // Réduire le niveau de z-index
-
+  },
+  scrollView: {
+    overflow: "hidden",
   },
   topBar: {
-    backgroundColor: '#5f5f5f',
-    position: 'absolute',
+    backgroundColor: "#5f5f5f",
+    position: "absolute",
     top: 0,
     left: 0,
     right: 0,
     height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 2,
   },
   videoContainer: {
-    width: '80%',
+    width: "80%",
     height: 160,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 100, // Ajout de la marge supérieure
   },
-  
+  ProfilUne: {
+    // position: "relative",
+    // PaddingHorizontal: 20,
+    alignItems: "center",
+  },
+
   video: {
-    width: '100%',
+    width: "100%",
     aspectRatio: 2,
   },
-  actionsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 10,
-  },
   actionButton: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     width: 80,
     height: 30,
-   
+
     borderRadius: 10,
     marginHorizontal: 5,
   },
@@ -132,21 +200,80 @@ const styles = StyleSheet.create({
     width: 30,
     height: 30,
   },
+  couronne: {
+    width: 40,
+    height: 40,
+    top: 80,
+    left: 0,
+    zIndex: 1,
+  },
+  logoducomte: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    top: 30,
+    marginVertical: 40,
+  },
+  starContainer: {
+    position: "absolute",
+    top: 80,
+    right: 80,
+  },
+  starShape: {
+    width: 70,
+    height: 70,
+    backgroundColor: "white",
+    // transform: [{ rotateZ: "-30deg" }],
+  },
+  starText: {
+    color: "#6441A4",
+    textAlign: "center",
+    marginTop: 10,
+  },
+  imageWithBorder: {
+    borderWidth: 5, // Épaisseur du contour
+    borderColor: "yellow", // Couleur du contour
+  },
   descriptionContainer: {
     marginTop: 10,
-    paddingHorizontal: 20,
+    width: screenWidth,
+    paddingHorizontal: 60,
+    paddingVertical: 20,
+    backgroundColor: "#ffffff",
   },
   descriptionText: {
-    color: 'white',
-    textAlign: 'center',
+    color: "#6441A4",
+    textAlign: "center",
+    fontSize: 10,
+    fontWeight: 700,
   },
-  twitchContainer: {
-    width: '100%',
-    height: 110,
-    backgroundColor: 'white',
-    borderRadius: 10,
+  borderSegment: {
+    height: 5,
+    position: "absolute",
+    bottom: 0,
+  },
+  scrollText: {
+    color: "white",
+    fontWeight: 500,
+    fontSize: 17,
     marginTop: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
+    marginBottom: -10,
+    marginLeft: 30,
+  },
+
+  twitchContainer: {
+    width: "90%",
+    marginHorizontal: "5%",
+    height: 90,
+    backgroundColor: "white",
+    borderRadius: 20,
+    marginTop: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  twitchIcon: {
+    borderRadius: 20,
+    height: 86,
+    marginLeft: 2,
   }, // Added the missing closing curly brace
 });
